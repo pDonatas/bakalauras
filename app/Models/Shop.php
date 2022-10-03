5fac841137c1a900096621abff2a79f3fd8a74dd
donatas <petrikauskas.donatas@gmail.com>
2022-10-03 20:26:16 +0300

    docker configuration + shops

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Shop extends Model
 {
     use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function clients(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'shop_clients', 'shop_id', 'client_id');
+    }
 }
