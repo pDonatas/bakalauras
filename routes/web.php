65e83ac19a50500d2c772a10725dc8ba8a0e9b89
donatas <petrikauskas.donatas@gmail.com>
2023-03-14 12:03:53 +0200

    ai test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CalendarController;
 use App\Http\Controllers\Admin\OrdersController;
+use App\Http\Controllers\AIController;
 use App\Http\Controllers\Main\MainController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,4 @@ require __DIR__ . '/admin.php';
 require __DIR__ . '/user.php';
 
 Route::post('appointments_ajax_update', [CalendarController::class, 'updateAjax'])->name('appointments_ajax_update');
+Route::get('image', [AIController::class, 'generateImage'])->name('image');
